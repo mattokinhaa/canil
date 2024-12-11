90af6d4fa02f7af78d8b9582b0958fd1cd6011ce
Gustavo Amaral Corrêa <Gubep@icloud.com>
2024-12-11 11:06:16 -0300

    ajustes popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,22 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval( function(){
+setInterval(function () {
     nextImage();
 }, 5000)
 
-function nextImage(){
+function nextImage() {
     count++;
-    if(count>4){
+    if (count > 4) {
         count = 1;
     }
 
-    document.getElementById("radio"+count).checked = true;
+    document.getElementById("radio" + count).checked = true;
 }
 
-function createPopup(id) {
-    let popupNode = document.querySelector(id);
-    let overlay = popupNode.querySelector(".overlay");
-    let closeBtn = popupNode.querySelector(".close-btn");
-    let popupContent = popupNode.querySelector(".popup-content");
-
-    // Função para abrir o pop-up
-    function openPopup() {
-        popupNode.classList.add("active");
-    }
-
-    // Função para fechar o pop-up
-    function closePopup() {
-        popupNode.classList.remove("active");
-    }
-
-    // Fechar o pop-up ao clicar no overlay
-    overlay.addEventListener("click", function(event) {
-        // Se o clique foi fora da área do conteúdo do pop-up, fecha
-        if (!popupContent.contains(event.target)) {
-            closePopup();
-        }
-    });
-
-    // Fechar o pop-up ao clicar no botão de fechar
-    closeBtn.addEventListener("click", closePopup);
-
-    return openPopup;
-}
-
-// Cria a função de abrir o pop-up com base no id do pop-up
-let popup = createPopup("#popup");
-
-// Adiciona o evento para abrir o pop-up quando o botão for clicado
-document.querySelector("#open-popup").addEventListener("click", popup);
+var modal = document.getElementById("popup");
+var btn = document.getElementById("open-popup");
+var span = document.getElementById("closeModalBtn");
+btn.onclick = function () { modal.style.display = "block"; }
+span.onclick = function () { modal.style.display = "none"; }
+window.onclick = function (event) { if (event.target == modal) { modal.style.display = "none"; } }
