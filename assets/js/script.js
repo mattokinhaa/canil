138e52ff7cca9f82d913560ac3209c30d0ac623e
Gustavo Amaral Corrêa <Gubep@icloud.com>
2024-12-12 15:56:35 -0300

    cards serviços responsivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,20 +14,56 @@ function nextImage() {
   document.getElementById("radio" + count).checked = true;
 }
 
-let currentIndex = 0;
+let currentIndex = 4;
 function moveCarousel(direction) {
-  const carousel = document.getElementById("carousel");
+  const carousel = document.getElementById("carousel-card");
   const totalCards = carousel.children.length;
   const cardWidth = carousel.children[0].offsetWidth;
   currentIndex += direction;
   if (currentIndex < 0) {
     currentIndex = totalCards - 1;
+    carousel.style.transition = "none";
+    carousel.style.transform = `translateX(${-currentIndex * cardWidth}px)`;
+    setTimeout(() => {
+      carousel.style.transition = "transform 0.5s ease";
+    }, 0);
   } else if (currentIndex >= totalCards) {
     currentIndex = 0;
+    carousel.style.transition = "none";
+    carousel.style.transform = `translateX(0px)`;
+    setTimeout(() => {
+      carousel.style.transition = "transform 0.5s ease";
+    }, 0);
+  } else if (currentIndex === 9) {
+    currentIndex = 4;
+    carousel.style.transition = "none";
+    carousel.style.transform = `translateX(${-currentIndex * cardWidth}px)`;
+    setTimeout(() => {
+      carousel.style.transition = "transform 0.5s ease";
+    }, 0);
+  } else {
+    const offset = -currentIndex * cardWidth;
+    carousel.style.transform = `translateX(${offset}px)`;
   }
-  const offset = -currentIndex * cardWidth;
-  carousel.style.transform = `translateX(${offset}px)`;
 }
+
+// Clonar os cards para criar o efeito de loop
+
+window.onload = function () {
+  const carousel = document.getElementById("carousel-card");
+  const cards = Array.from(carousel.children);
+  for (let i = 0; i < 2; i++) {
+    cards.forEach((card) => {
+      const clone = card.cloneNode(true);
+      carousel.appendChild(clone);
+    });
+  }
+
+  //Ajustar a posição inicial do carrossel
+
+  const initialOffset = -currentIndex * carousel.children[0].offsetWidth;
+  carousel.style.transform = `translateX(${initialOffset}px)`;
+};
 
 var modal = document.getElementById("popup");
 var btn = document.getElementById("open-popup");
